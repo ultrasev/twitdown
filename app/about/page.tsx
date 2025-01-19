22c7ee4d6b978d2f9f640412546d77432b663f13
ultrasev <i@ultrasev.com>
2025-01-19 17:57:56 +0800

    style: Adjust About page styling

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -18,11 +18,13 @@ export const metadata: Metadata = {
 
 function AboutPage() {
   return (
-    <article className="max-w-6xl mx-auto px-16 py-12 min-h-screen rounded-xl text-lg bg-gradient-to-b from-amber-50/50 to-transparent">
+    <article className="max-w-5xl mx-auto px-16 py-12 min-h-screen rounded-xl text-lg bg-gradient-to-b from-amber-50/50 to-transparent">
       <div className="prose max-w-none">
-        <h1 className="text-4xl font-bold mb-8 bg-clip-text text-transparent bg-gradient-to-r from-amber-700 to-amber-950">
-          About TwitDown
-        </h1>
+        <div className="text-center mb-16">
+          <h1 className="text-5xl font-serif font-bold text-stone-800">
+            About TwitDown
+          </h1>
+        </div>
 
         <p className="text-xl mb-6">
           TwitDown was developed to streamline the process of downloading

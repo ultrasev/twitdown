e58f630c5b2af8b20d92504c310819db9d38c906
ultrasev <i@ultrasev.com>
2025-01-19 21:19:20 +0800

    chore: update .gitignore to include new files

--- app/dashboard/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="w-full h-96 flex items-center justify-center">
-      <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900 dark:border-gray-100" />
-    </div>
-  );
-}
